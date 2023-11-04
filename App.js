5ec7de22f8707f4c1f4821d0ab6e3df62b3eaabc
vatsalkudecha <1311vatsal@gmail.com>
2023-11-04 19:47:11 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,42 @@
+// creating element is core react thing
+// const heading = React.createElement(
+//     "h1",
+//     { id: "heading", xyz: "abc" },
+//     "Hello world from React!"
+// );
+
+// console.log(heading)
+
+/*
+<div id="parent">
+    <div id="child">
+        <h1>I am 1st h1 tag<h1>
+        <h1>I am 2nd h1 tag<h1>
+    </div>
+</div>    
+
+ React.createElement(React Object) => .render converts to => HTML(Browser understands)
+
+ If you want to create sibling, instead of passing single child to attribute, 
+ put multiple childs in an array
+*/
+
+const parent = React.createElement(
+    "div",
+    { id: "parent" },
+    React.createElement(
+        "div",
+        { id: "child" },
+        [React.createElement("h1", {}, "I am 1st h1 tag"),
+         React.createElement("h1", {}, "I am 2nd h1 tag")
+        ]
+    )
+);
+
+// when we want to put this element in react dom we use ReactDOM
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+console.log(parent);    // (object)
+
+// render
+root.render(parent);
